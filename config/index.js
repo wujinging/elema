7b08c4d1039daf426e656f56b3c8f745862a5603
wujinging <1689611800@qq.com>
2018-07-19 11:43:48 +0800

    test

--- config/index.js
+++ config/index.js
@@ -42,7 +42,9 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+    // assetsSubDirectory: 'static',
+    // assetsPublicPath: '/',
+    assetsSubDirectory: 'dist/static',
     assetsPublicPath: '',
 
     /**
